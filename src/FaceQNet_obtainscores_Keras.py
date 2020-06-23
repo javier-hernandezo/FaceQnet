3ee445584ffd9535f219f25ee236d8325a9d7e89
Javier Hernandez Ortega <40597519+uam-biometrics@users.noreply.github.com>
2020-06-23 12:19:09 +0200

    Update FaceQNet_obtainscores_Keras.py

--- src/FaceQNet_obtainscores_Keras.py
+++ src/FaceQNet_obtainscores_Keras.py
@@ -13,15 +13,7 @@ def load_test():
 	images_names = []
 	image_path = r".\\Samples_cropped\\"
 	print('Read test images')
-	# for f in [f for f in os.listdir(image_path) if os.path.isdir(os.path.join(image_path, f))]:
-		# carpeta= os.path.join(image_path, f)
-		# print(carpeta)
-		# for imagen in [imagen for imagen in os.listdir(carpeta) if os.path.isfile(os.path.join(carpeta, imagen))]:
-			# imagenes = os.path.join(carpeta, imagen)
-			# img = cv2.resize(cv2.imread(imagenes, cv2.IMREAD_COLOR), (224, 224))
-			# X_test.append(img)
-			# images_names.append(imagenes)
-			
+	
 	for imagen in [imagen for imagen in os.listdir(image_path) if os.path.isfile(os.path.join(image_path, imagen))]:
 		imagenes = os.path.join(image_path, imagen)
 		print(imagenes)
@@ -33,50 +25,37 @@ def load_test():
 def read_and_normalize_test_data():
     test_data, images_names = load_test()
     test_data = np.array(test_data, copy=False, dtype=np.float32)
-	
     return test_data, images_names
 		
 
-#Loading the pretrained model
-model = load_model('FaceQnet.h5')
+#Loading one of the the pretrained models
+
+# model = load_model('FaceQnet.h5')
+
+model = load_model('FaceQnet_v1.h5')
 
 #See the details (layers) of FaceQnet
 # print(model.summary())
 
 #Loading the test data
 test_data, images_names = read_and_normalize_test_data()
-# if test_data.ndim == 4:
-	# axis = (1, 2, 3)
-	# size = test_data[0].size
-# elif test_data.ndim == 3:
-	# axis = (0, 1, 2)
-	# size = test_data.size
-# else:
-	# raise ValueError('Dimension should be 3 or 4')
-
-# mean = np.mean(test_data, axis=axis, keepdims=True)
-# std = np.std(test_data, axis=axis, keepdims=True)
-# std_adj = np.maximum(std, 1.0/np.sqrt(size))
-# y = (test_data - mean) / std_adj
-
 y=test_data
 
 #Extract quality scores for the samples
-m=0.7
-s=0.5
 score = model.predict(y, batch_size=batch_size, verbose=1)
-#score = 0.5*np.tanh(((score-m)/s) + 1)
-#predictions = -score + 1; #Convertimos el score de distancia en similitud
-predictions = score
-#Guardamos los scores para cada clase en la prediccion de scores
+predictions = score ;
+
+
+#Saving the quality measures for the test images
 fichero_scores = open('scores_quality.txt','w')
 i=0
+
 
 #Saving the scores in a file
 fichero_scores.write("img;score\n")
 for item in predictions:
-	fichero_scores.write("%s" % images_names[i]) #fichero
-	#Contraining the output scores to the 0-1 range
+	fichero_scores.write("%s" % images_names[i])
+	#Constraining the output scores to the 0-1 range
 	#0 means worst quality, 1 means best quality
 	if float(predictions[i])<0:
 		predictions[i]='0'
